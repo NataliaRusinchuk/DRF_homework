032846f5dfca071de2756c7acc1353792fa96a85
NataliaRusinchuk <n.m.rusinchuk@gmail.com>
2022-09-01 15:28:07 +0200

    add permissions
    Signed-off-by: NataliaRusinchuk <n.m.rusinchuk@gmail.com>

--- api/v1/views.py
+++ api/v1/views.py
@@ -1,10 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
-from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticatedOrReadOnly
+from rest_framework import filters, generics
+from rest_framework.decorators import permission_classes
 from products.models import Product, Order
 from .serializer import ProductSerializer, OrderSerializer
  
-    
+@permission_classes([AllowAny])    
 class ProductListView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -14,27 +15,27 @@ class ProductListView(generics.ListAPIView):
         filters.SearchFilter
     ]
 
-
+@permission_classes([AllowAny]) 
 class ProductObject(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-
+@permission_classes([IsAdminUser]) 
 class ProductObjectDelete(generics.RetrieveDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-
+@permission_classes([IsAdminUser]) 
 class ProductObjectChange(generics.RetrieveUpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-
+@permission_classes([IsAdminUser])
 class ProductCreate(generics.CreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-
+@permission_classes([IsAuthenticatedOrReadOnly])
 class OrderView(generics.ListCreateAPIView):
     queryset = Order.objects.all().select_related('product')
     serializer_class = OrderSerializer
